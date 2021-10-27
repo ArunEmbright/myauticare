95cbf8861bd86f8274ff3bf8071693919833b8a4
arunlajayan <arunlajayan@gmail.com>
2021-10-27 14:52:30 +0530

    mailjs integrate

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,17 +1,19 @@
 function sendMail() {
-    var payload = {
-      name: document.getElementById("name").value,
-      mail: document.getElementById("mail").value,
-      
-      subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value,
-    };
-    emailjs.send("service_x7wi6cl", "template_01ovtlw", payload).then(
-      function (response) {
-        alert("Mail has been sent successfully");
-      },
-      function (error) {
-        alert("Couldn't send the mail due to some error");
-      }
-    );
-  }
+  console.log("on!")
+  var payload = {
+    name: document.getElementById("name").value,
+    mail: document.getElementById("email").value,
+    phone: document.getElementById("number").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs.send("service_x7wi6cl", "template_01ovtlw", payload).then(
+    function (response) {
+      console.log("hai")
+      alert("Mail has been sent successfully");
+    },
+    function (error) {
+      alert("Couldn't send the mail due to some error");
+    }
+  );
+}
